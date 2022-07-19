843a8941fcdd663003fb8fc631dc389286c90fd4
PrimaryGarbage <artstudioexp@gmail.com>
2022-07-19 17:08:09 +0300

    Start loadScene() implementation

--- src/scene_manager.cpp
+++ src/scene_manager.cpp
@@ -2,16 +2,17 @@
 #include "prim_exception.hpp"
 #include <fstream>
 #include <filesystem>
+#include <sstream>
 #include "node.hpp"
 
 namespace prim
 {
     namespace fs = std::filesystem;
-   
+
     SceneManager::SceneManager(const char* savePath) : savePath(savePath)
     {
         fs::path path(savePath);
-        if(!fs::exists(path))
+        if (!fs::exists(path))
         {
             fs::create_directories(path);
         }
@@ -20,15 +21,27 @@ namespace prim
 
     void SceneManager::loadScene(std::string fileName, Node* parentNode)
     {
+        fs::path path(savePath + fileName + sceneFileExtension);
+        if (!fs::exists(path)) throw PRIM_EXCEPTION("File with the path '" + path.string() + "' doesn't exists.");
+        
+        std::ifstream stream(path.string(), std::ios::in);
+        if (!stream.good()) throw PRIM_EXCEPTION("Unable to open file stream.");
+
+        std::stringstream ss;
+        ss << stream.rdbuf();
+        std::string data = std::move(ss.str());
+
+
+        stream.close();
     }
-    
+
     void SceneManager::saveScene(Node* scene, std::string fileName, bool ovewrite)
     {
         fs::path path(savePath + fileName + sceneFileExtension);
-        if(fs::exists(path) && !ovewrite) throw PRIM_EXCEPTION("File with the path '" + path.string() + "' already exists.");
+        if (fs::exists(path) && !ovewrite) throw PRIM_EXCEPTION("File with the path '" + path.string() + "' already exists.");
 
         std::ofstream stream(path.string(), std::ios::out | std::ios::trunc);
-        if(!stream.good()) throw PRIM_EXCEPTION("Unable to open file stream.");
+        if (!stream.good()) throw PRIM_EXCEPTION("Unable to open file stream.");
 
         stream << scene->serialize();
 
